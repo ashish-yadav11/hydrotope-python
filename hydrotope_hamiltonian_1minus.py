41e8fa2da6b26d1b4d3c3769bcfb0f44a44115b7
Ashish Kumar Yadav <ashish11yadav01@gmail.com>
2026-08-09 06:46:57 +0530

    Rewrote hydrotope_hamiltonian_1minus from hydrotope_hamiltonian
    Only one fact has been used for getting hydrotope_hamiltonian_1minus
    from hydrotope_hamiltonian: all internal momenta are -ve (taking the -ve
    k to be the root in k's), and Ekernel() vanishes for 1 -ve ks if the -ve
    k is in the first two slots... This fact allows the Propagator to be
    summed nicely for the + and - branches allowing the Vertex contributions
    to be separable with just the right compensatory factors...

--- hydrotope_hamiltonian_1minus.py
+++ hydrotope_hamiltonian_1minus.py
@@ -1,29 +1,16 @@
 from __future__ import annotations
-from math import factorial, prod, sqrt, pi
-from sympy import symbols, limit, Expr, Rational, I
+from math import factorial, prod
+from sympy import Expr, Rational, I
 from functools import lru_cache
-from itertools import combinations, permutations, product
+from itertools import combinations, permutations
 from typing import List, Tuple, Iterator, Callable
 
 
 g = Rational(1)
 
 
-def omega(k):
-    return sqrt(g*abs(k))
-
-
-def Vertexc(n: int, ks: List[Rational], ws: List[Rational]) -> Expr:
-    return I * (-1)**(n-1) * factorial(n-3) # (-i)**2 {from mode expansion of ψ} * (-1) {factor in front in (7) 2019ussem 2/4}
-
-
-def Propagatorc(k: Rational, w: Rational) -> Expr:
-    wk = omega(k)
-
-    if w**2 == wk**2:
-        print('Warning: internal resonance detected!')
-
-    return -I * k**2 / (w**2 - wk**2)
+def omegasqr(k: Rational) -> Rational:
+    return g*abs(k)
 
 
 # generates all set partitions of S into k non-empty parts
@@ -46,6 +33,39 @@ def SetPartitions(S: List[int], k: int) -> Iterator[Tuple[Tuple[int, ...], ...]]
     for s in SetPartitions(S[1:], k):
         for i in range(len(s)):
             yield s[:i] + ((s0,) + s[i],) + s[i+1:]
+
+
+def Ekernel(n: int, ks: List[Rational]) -> Rational:
+    if n == 3:
+        return -(abs(ks[0])*abs(ks[1]) + ks[0]*ks[1]) / 2
+
+    val = abs(ks[1])**(n-3) * Ekernel(3, [ks[0], ks[1], sum(ks[2:])]) / factorial(n-2)
+
+    for m in range(1, n-2): # m goes upto n-3
+        val -= (abs(ks[1])**m / factorial(m)) * Ekernel(n-m, [ks[0], sum(ks[1:m+2])] + ks[m+2:])
+
+    return val
+
+
+def Vertex(n: int, ks: List[Rational], ws: List[Rational]) -> Expr:
+    val = Rational(0)
+
+    for p in permutations(range(n)):
+        val +=  (ws[p[0]] / abs(ks[p[0]])) * (ws[p[1]] / abs(ks[p[1]])) * Ekernel(n, [ks[i] for i in p])
+#       if ksp[0] >= 0 and ksp[1] >= 0:
+#           val += (ws[p[0]] / abs(ks[p[0]])) * (ws[p[1]] / abs(ks[p[1]]))
+
+    return -I * val
+#   return I * (-1)**(n-1) * prod([k for k in ks if k >= 0]) * val / (n-2) # (-i)**2 {from mode expansion of ψ} * (-1) {factor in front in (7) 2019ussem 2/4}
+
+
+def Propagator(k: Rational, w: Rational) -> Expr:
+    wksqr = omegasqr(k)
+
+    if w**2 == wksqr:
+        print('Warning: internal resonance detected!')
+
+    return -I * abs(k) / (w**2 - wksqr)
 
 
 def BGcurrent(ks: List[Rational], ws: List[Rational]) -> Callable[[Tuple[int, ...]], Expr]:
@@ -71,10 +91,9 @@ def BGcurrent(ks: List[Rational], ws: List[Rational]) -> Callable[[Tuple[int, ..
                 for p in prt:
                     curprod *= current(tuple(p))
 
-                wbyk = [w/k for k,w in zip(kps, wps)]
-                val += Vertexc(m+1, [-kr] + kps, [-wr] + wps) * (sum(wbyk)**2 - sum(x**2 for x in wbyk)) * curprod
+                val += Vertex(m+1, [-kr] + kps, [-wr] + wps) * curprod
 
-        return Propagatorc(-kr, wr) * val
+        return Propagator(-kr, wr) * val
 
     return current
 
@@ -95,12 +114,17 @@ def BGamplitude(ks: List[Rational], ws: List[Rational]) -> Expr:
             for p in prt:
                 curprod *= current(tuple(p))
 
-            wbyk = [w/k for k,w in zip(kps, wps)]
-            val += Vertexc(m+1, [ks[0]] + kps, [ws[0]] + wps) * (sum(wbyk)**2 - sum(x**2 for x in wbyk)) * curprod
+            amp = Vertex(m+1, [ks[0]] + kps, [ws[0]] + wps) * curprod
+            print('amplitude', amp)
+            val += amp
 
     return val
 
 
+# given n signs ss = [σ(1), σ(2), ..., σ(n-1), σ(n) = -σ(1)], and n-2
+# frequencies wf = [ω(2), ..., ω(n-1)]; create on-shell momenta and frequencies
+# (kᵢ = σᵢωᵢ², ωᵢ), 1 ≤ i ≤ n with Σkᵢ = Σωᵢ = 0. [the restriction σ(n) = -σ(1)
+# is only for convenience—it leads to a unique solution.]
 def MakeKinematics(wf: List[Rational]) -> Tuple[List[Rational], List[Rational]]:
     ss = [-1] + [1] * (len(wf) + 1)
 
